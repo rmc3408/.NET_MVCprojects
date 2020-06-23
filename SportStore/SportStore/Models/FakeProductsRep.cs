620a97d78b0e619268c32455fdbcfb62e7d8ba6e
rmc3408 <rmc3408@protonmail.com>
2020-06-23 12:59:02 -0400

    video2

---
+++ SportStore/SportStore/Models/FakeProductsRep.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportStore.Models
+{
+    public class FakeProductsRep : IProductRepository
+    {
+        public IQueryable<Product> PackProducts => new List<Product>
+        {
+            new Product { Name = "Football",Price = 25.50 },
+            new Product { Name = "Soccer",Price = 179 },
+            new Product { Name = "Running shoes",Price = 95.99 }
+
+        }.AsQueryable<Product>();
+
+
+    }
+}
--- SportStore/SportStore/Startup.cs
+++ SportStore/SportStore/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using SportStore.Models;
+
 
 namespace SportStore
 {
@@ -15,20 +17,22 @@ namespace SportStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IProductRepository,FakeProductsRep>();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
+            
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+            app.UseStatusCodePages();
+            app.UseMvc(routes => { 
+            
             });
+            
+            
         }
     }
 }
