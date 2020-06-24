220c0968cd3eebeb503eba62b42ca027c80357fd
rmc3408 <rmc3408@protonmail.com>
2020-06-23 23:40:39 -0400

    week03

--- SportStore/SportStore/Models/FakeProductsRep.cs
+++ SportStore/SportStore/Models/FakeProductsRep.cs
@@ -5,13 +5,15 @@ using System.Threading.Tasks;
 
 namespace SportStore.Models
 {
+    //this is a hardCoded List, replace database.
+
     public class FakeProductsRep : IProductRepository
     {
         public IQueryable<Product> PackProducts => new List<Product>
         {
-            new Product { Name = "Football",Price = 25.50 },
-            new Product { Name = "Soccer",Price = 179 },
-            new Product { Name = "Running shoes",Price = 95.99 }
+            new Product { Name = "Football", Price = 25.50 },
+            new Product { Name = "Soccer", Price = 179.02 },
+            new Product { Name = "Running shoes", Price = 95.99 }
 
         }.AsQueryable<Product>();
 
--- SportStore/SportStore/Models/IProductRepository.cs
+++ SportStore/SportStore/Models/IProductRepository.cs
@@ -5,8 +5,14 @@ using System.Threading.Tasks;
 
 namespace SportStore.Models
 {
+    // not concrete class... Do not create object//
     public interface IProductRepository
     {
+        /*
+         * It has as good performance to Database. filter data.
+         * dont download whole object to the memory. 
+         * select filtered data */
+
         IQueryable<Product> PackProducts { get; }
 
     }
