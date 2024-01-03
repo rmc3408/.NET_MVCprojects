4d035ea41609c580f84ba90bbb1cb2d4eafbde50
Raphael <rmc3408@protonmail.com>
2024-01-02 22:41:08 -0500

    rename mvc-beginner

--- NTierModels/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace bhrugen_patel.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
